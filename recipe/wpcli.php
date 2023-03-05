dbfa2aac48afecfce8304108e5453b1a8e866d24
Charles Dupoiron <charles.d@yeswedev.bzh>
2023-03-05 19:06:37 +0100

    feat(wpcli) new maintenance and acorn WP-CLI tasks

---
+++ recipe/wpcli.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Deployer;
+
+// Activate maintenance
+set('bin/wp', 'wp');
+
+desc('Activate maintenance mode');
+task(
+    'maintenance:activate',
+    function () {
+        run("cd {{release_path}} && {{bin/wp}} maintenance-mode activate");
+        info(run("cd {{release_path}} && {{bin/wp}} maintenance-mode status"));
+    }
+);
+
+// Deactivate maintenance
+desc('Deactivate maintenance mode ');
+task(
+    'maintenance:deactivate',
+    function () {
+        run("cd {{release_path}} && {{bin/wp}} maintenance-mode deactivate");
+        info(run("cd {{release_path}} && {{bin/wp}} maintenance-mode status"));
+    }
+);
+
+task(
+    'acorn:discover',
+    function () {
+        run(
+            "cd /home/yeswedev/avf-biomedical.git/releases/5 && '.
+            '{{bin/wp}} acorn package:discover && {{bin/wp}} acorn optimize"
+        );
+    }
+)->desc('Discover acorn packages');
